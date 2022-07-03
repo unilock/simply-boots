f31eb30ef46ae66fb8394be764a66664627a2b40
loganf55 <loganf55@uw.edu>
2022-07-03 15:31:26 -0700

    Fix improper identifier for ice skates tag

--- src/main/java/io/github/alabasteralibi/simplyboots/registry/SimplyBootsTags.java
+++ src/main/java/io/github/alabasteralibi/simplyboots/registry/SimplyBootsTags.java
@@ -10,5 +10,5 @@ public class SimplyBootsTags {
     public static final Tag<Item> HOT_FLUID_WALKING_BOOTS = TagFactoryImpl.ITEM.create(new Identifier("simplyboots", "hot_fluid_walking_boots"));
     public static final Tag<Item> FIRE_RESISTANT_BOOTS = TagFactoryImpl.ITEM.create(new Identifier("simplyboots", "fire_resistant_boots"));
     public static final Tag<Item> ROCKET_BOOTS = TagFactoryImpl.ITEM.create(new Identifier("simplyboots", "rocket_boots"));
-    public static final Tag<Item> ICE_SKATE_BOOTS = TagFactoryImpl.ITEM.create(new Identifier("simplyboots", "rocket_boots"));
+    public static final Tag<Item> ICE_SKATE_BOOTS = TagFactoryImpl.ITEM.create(new Identifier("simplyboots", "ice_skate_boots"));
 }
