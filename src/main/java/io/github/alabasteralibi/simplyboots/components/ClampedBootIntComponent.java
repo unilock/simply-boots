5cf906474e0d2662bb47a1937063ef8fbf372972
loganf55 <loganf55@uw.edu>
2022-07-03 15:28:00 -0700

    Add Javadoc to ClampedBootIntComponent interface

--- src/main/java/io/github/alabasteralibi/simplyboots/components/ClampedBootIntComponent.java
+++ src/main/java/io/github/alabasteralibi/simplyboots/components/ClampedBootIntComponent.java
@@ -3,7 +3,19 @@ package io.github.alabasteralibi.simplyboots.components;
 import dev.onyxstudios.cca.api.v3.component.Component;
 
 public interface ClampedBootIntComponent extends Component {
+    /**
+     * Gets this component's value.
+     * @return An integer representing the current value of the component.
+     */
     int getValue();
+
+    /**
+     * Increments the value of this component by one. Will not go over the component's maximum value.
+     */
     void increment();
+
+    /**
+     * Decrements the value of this component by one. Will not go under the component's minimum value.
+     */
     void decrement();
 }
