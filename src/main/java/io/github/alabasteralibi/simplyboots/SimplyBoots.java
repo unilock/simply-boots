0f691508b9cd18b5e8957116445196772e72cc41
Loganf55UW <loganf55@uw.edu>
2023-12-22 23:44:36 -0800

    Move HUD rendering to client initializer

--- src/main/java/io/github/alabasteralibi/simplyboots/SimplyBoots.java
+++ src/main/java/io/github/alabasteralibi/simplyboots/SimplyBoots.java
@@ -1,26 +1,17 @@
 package io.github.alabasteralibi.simplyboots;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.alabasteralibi.simplyboots.components.BootComponents;
 import io.github.alabasteralibi.simplyboots.components.ClampedBootIntComponent;
-import io.github.alabasteralibi.simplyboots.components.LavaBootsComponent;
-import io.github.alabasteralibi.simplyboots.components.RocketBootsComponent;
 import io.github.alabasteralibi.simplyboots.registry.SimplyBootsItems;
 import io.github.alabasteralibi.simplyboots.registry.SimplyBootsTags;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.loot.v2.LootTableSource;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -31,18 +22,14 @@ import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.VillagerProfession;
 
 public class SimplyBoots implements ModInitializer {
-    public static final Identifier ICONS_TEXTURE = SimplyBootsHelpers.id("textures/hud/icons.png");
-
     public static final ItemGroup MAIN_GROUP = Registry.register(
             Registries.ITEM_GROUP,
             SimplyBootsHelpers.id("main_group"),
@@ -60,7 +47,6 @@ public class SimplyBoots implements ModInitializer {
         ));
 
         LootTableEvents.MODIFY.register(this::setupLootTableAdditions);
-        HudRenderCallback.EVENT.register(this::setupHudAdditions);
 
         // Registers a packet for when clients wish to activate their rocket boots
         ServerPlayNetworking.registerGlobalReceiver(SimplyBootsHelpers.id("rocket_boost"),
@@ -134,86 +120,5 @@ public class SimplyBoots implements ModInitializer {
                 tableBuilder.pool(pool);
             }
         }
-    }
-
-    // Render HUD elements for rocket and lava timers
-    private void setupHudAdditions(DrawContext context, float tickDelta) {
-        PlayerEntity player = MinecraftClient.getInstance().player;
-
-        if (player == null || player.isCreative()) {
-            return;
-        }
-
-        int vehicle_hearts = 0;
-        if (player.getVehicle() instanceof LivingEntity vehicle) {
-            vehicle_hearts = (int) (vehicle.getMaxHealth() + 0.5F) / 2;
-            if (vehicle_hearts > 30) {
-                vehicle_hearts = 30;
-            }
-        }
-
-        int drawWidth = MinecraftClient.getInstance().getWindow().getScaledWidth() / 2 + 91;
-        int drawHeight = MinecraftClient.getInstance().getWindow().getScaledHeight() - 59;
-        drawHeight -= ((int) Math.ceil(vehicle_hearts / 10.0D) - 1) * 10;
-        drawHeight -= player.getAir() < player.getMaxAir() ? 10 : 0;  // If air showing, move up
-
-        drawHeight -= renderLavaImmunityBar(context, drawWidth, drawHeight, player) ? 10 : 0;
-        drawHeight -= renderRocketBar(context, drawWidth, drawHeight, player) ? 10 : 0;
-    }
-
-    /**
-     * Renders the lava immunity bar for the player above the hunger shanks.
-     *
-     * @param context     The DrawContext to draw with.
-     * @param drawWidth   The width to draw the right side of the bar at.
-     * @param drawHeight  The height to draw the bar at.
-     * @return Whether the bar was successfully rendered or not.
-     */
-    private boolean renderLavaImmunityBar(DrawContext context, int drawWidth, int drawHeight, PlayerEntity player) {
-        // Don't display if full (or empty) and not in lava (like breath bubbles)
-        int lavaTicksLeft = BootComponents.LAVA_BOOTS.get(player).getValue();
-        if (lavaTicksLeft == LavaBootsComponent.MAX_VALUE && !player.isSubmergedIn(FluidTags.LAVA)) {
-            return false;
-        }
-        if (lavaTicksLeft == LavaBootsComponent.MIN_VALUE) {
-            return false;
-        }
-
-        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
-        int iconsToDraw = MathHelper.ceil(lavaTicksLeft * 10 / (double) LavaBootsComponent.MAX_VALUE);
-        for (int i = 0; i < iconsToDraw; ++i) {
-            context.drawTexture(ICONS_TEXTURE, drawWidth - i * 8 - 9, drawHeight, 0, 0, 9, 9, 18, 9);
-        }
-        return true;
-    }
-
-    /**
-     * Renders the remaining rocket boost time above any other bars.
-     *
-     * @param context     The DrawContext to draw with.
-     * @param drawWidth   The width to draw the right side of the bar at.
-     * @param drawHeight  The height to draw the bar at.
-     * @return Whether the rocket bar was successfully rendered or not.
-     */
-    private boolean renderRocketBar(DrawContext context, int drawWidth, int drawHeight, PlayerEntity player) {
-        // Don't display if full or empty
-        int rocketTicksLeft = BootComponents.ROCKET_BOOTS.get(player).getValue();
-        if (rocketTicksLeft == RocketBootsComponent.MAX_VALUE || player.isOnGround()) {
-            return false;
-        }
-        if (rocketTicksLeft == RocketBootsComponent.MIN_VALUE) {
-            return false;
-        }
-
-        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
-        int iconsToDraw = MathHelper.ceil(rocketTicksLeft * 10 / (double) RocketBootsComponent.MAX_VALUE);
-        for (int i = 0; i < iconsToDraw; ++i) {
-            context.drawTexture(ICONS_TEXTURE, drawWidth - i * 8 - 9, drawHeight, 9, 0, 9, 9, 18, 9);
-        }
-        return true;
     }
 }
--- src/main/java/io/github/alabasteralibi/simplyboots/SimplyBootsClient.java
+++ src/main/java/io/github/alabasteralibi/simplyboots/SimplyBootsClient.java
@@ -1,26 +1,110 @@
 package io.github.alabasteralibi.simplyboots;
 
-import dev.emi.trinkets.api.client.TrinketRendererRegistry;
-import io.github.alabasteralibi.simplyboots.client.TrinketBootRenderer;
+import com.mojang.blaze3d.systems.RenderSystem;
+import io.github.alabasteralibi.simplyboots.components.BootComponents;
+import io.github.alabasteralibi.simplyboots.components.LavaBootsComponent;
+import io.github.alabasteralibi.simplyboots.components.RocketBootsComponent;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-
-import static io.github.alabasteralibi.simplyboots.registry.SimplyBootsItems.*;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
 public class SimplyBootsClient implements ClientModInitializer {
+    public static final Identifier ICONS_TEXTURE = SimplyBootsHelpers.id("textures/hud/icons.png");
+
     @Override
     public void onInitializeClient() {
-        TrinketRendererRegistry.registerRenderer(HERMES_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(ROCKET_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(SPECTRE_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(LIGHTNING_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(ICE_SKATES, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(FROSTSPARK_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(WATER_WALKING_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(OBSIDIAN_WATER_WALKING_BOOTS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(LAVA_WADERS, new TrinketBootRenderer());
-        TrinketRendererRegistry.registerRenderer(TERRASPARK_BOOTS, new TrinketBootRenderer());
+        HudRenderCallback.EVENT.register(this::setupHudAdditions);
+    }
+
+    // Render HUD elements for rocket and lava timers
+    private void setupHudAdditions(DrawContext context, float tickDelta) {
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+        if (player == null || player.isCreative()) {
+            return;
+        }
+
+        int vehicle_hearts = 0;
+        if (player.getVehicle() instanceof LivingEntity vehicle) {
+            vehicle_hearts = (int) (vehicle.getMaxHealth() + 0.5F) / 2;
+            if (vehicle_hearts > 30) {
+                vehicle_hearts = 30;
+            }
+        }
+
+        int drawWidth = MinecraftClient.getInstance().getWindow().getScaledWidth() / 2 + 91;
+        int drawHeight = MinecraftClient.getInstance().getWindow().getScaledHeight() - 59;
+        drawHeight -= ((int) Math.ceil(vehicle_hearts / 10.0D) - 1) * 10;
+        drawHeight -= player.getAir() < player.getMaxAir() ? 10 : 0;  // If air showing, move up
+
+        drawHeight -= renderLavaImmunityBar(context, drawWidth, drawHeight, player) ? 10 : 0;
+        drawHeight -= renderRocketBar(context, drawWidth, drawHeight, player) ? 10 : 0;
+    }
+
+    /**
+     * Renders the lava immunity bar for the player above the hunger shanks.
+     *
+     * @param context    The DrawContext to draw with.
+     * @param drawWidth  The width to draw the right side of the bar at.
+     * @param drawHeight The height to draw the bar at.
+     * @return Whether the bar was successfully rendered or not.
+     */
+    private boolean renderLavaImmunityBar(DrawContext context, int drawWidth, int drawHeight, PlayerEntity player) {
+        // Don't display if full (or empty) and not in lava (like breath bubbles)
+        int lavaTicksLeft = BootComponents.LAVA_BOOTS.get(player).getValue();
+        if (lavaTicksLeft == LavaBootsComponent.MAX_VALUE && !player.isSubmergedIn(FluidTags.LAVA)) {
+            return false;
+        }
+        if (lavaTicksLeft == LavaBootsComponent.MIN_VALUE) {
+            return false;
+        }
+
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
+        int iconsToDraw = MathHelper.ceil(lavaTicksLeft * 10 / (double) LavaBootsComponent.MAX_VALUE);
+        for (int i = 0; i < iconsToDraw; ++i) {
+            context.drawTexture(ICONS_TEXTURE, drawWidth - i * 8 - 9, drawHeight, 0, 0, 9, 9, 18, 9);
+        }
+        return true;
+    }
+
+    /**
+     * Renders the remaining rocket boost time above any other bars.
+     *
+     * @param context    The DrawContext to draw with.
+     * @param drawWidth  The width to draw the right side of the bar at.
+     * @param drawHeight The height to draw the bar at.
+     * @return Whether the rocket bar was successfully rendered or not.
+     */
+    private boolean renderRocketBar(DrawContext context, int drawWidth, int drawHeight, PlayerEntity player) {
+        // Don't display if full or empty
+        int rocketTicksLeft = BootComponents.ROCKET_BOOTS.get(player).getValue();
+        if (rocketTicksLeft == RocketBootsComponent.MAX_VALUE || player.isOnGround()) {
+            return false;
+        }
+        if (rocketTicksLeft == RocketBootsComponent.MIN_VALUE) {
+            return false;
+        }
+
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, ICONS_TEXTURE);
+        int iconsToDraw = MathHelper.ceil(rocketTicksLeft * 10 / (double) RocketBootsComponent.MAX_VALUE);
+        for (int i = 0; i < iconsToDraw; ++i) {
+            context.drawTexture(ICONS_TEXTURE, drawWidth - i * 8 - 9, drawHeight, 9, 0, 9, 9, 18, 9);
+        }
+        return true;
     }
 }
