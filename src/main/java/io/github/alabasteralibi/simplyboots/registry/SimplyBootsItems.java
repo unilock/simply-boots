1bd1df30e213940c242de4c2937c935b7c746f0d
Loganf55UW <loganf55@uw.edu>
2023-12-24 03:11:01 -0800

    Add item rarities

--- src/main/java/io/github/alabasteralibi/simplyboots/registry/SimplyBootsItems.java
+++ src/main/java/io/github/alabasteralibi/simplyboots/registry/SimplyBootsItems.java
@@ -14,23 +14,24 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
+import net.minecraft.util.Rarity;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public class SimplyBootsItems {
-    public static final Item HERMES_BOOTS = register("hermes_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item ROCKET_BOOTS = register("rocket_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item SPECTRE_BOOTS = register("spectre_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item LIGHTNING_BOOTS = register("lightning_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item ICE_SKATES = register("ice_skates", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item FROSTSPARK_BOOTS = register("frostspark_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item WATER_WALKING_BOOTS = register("water_walking_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item OBSIDIAN_WATER_WALKING_BOOTS = register("obsidian_water_walking_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item LAVA_WADERS = register("lava_waders", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().fireproof()));
-    public static final Item TERRASPARK_BOOTS = register("terraspark_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().fireproof()));
-    public static final Item LAVA_CHARM = register("lava_charm", new Item(new FabricItemSettings().fireproof().maxCount(1)) {
+    public static final Item HERMES_BOOTS = register("hermes_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.UNCOMMON)));
+    public static final Item ROCKET_BOOTS = register("rocket_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.UNCOMMON)));
+    public static final Item SPECTRE_BOOTS = register("spectre_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.RARE)));
+    public static final Item LIGHTNING_BOOTS = register("lightning_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.RARE)));
+    public static final Item ICE_SKATES = register("ice_skates", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.UNCOMMON)));
+    public static final Item FROSTSPARK_BOOTS = register("frostspark_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.RARE)));
+    public static final Item WATER_WALKING_BOOTS = register("water_walking_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.UNCOMMON)));
+    public static final Item OBSIDIAN_WATER_WALKING_BOOTS = register("obsidian_water_walking_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.RARE)));
+    public static final Item LAVA_WADERS = register("lava_waders", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().fireproof().rarity(Rarity.RARE)));
+    public static final Item TERRASPARK_BOOTS = register("terraspark_boots", new BootItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new FabricItemSettings().fireproof().rarity(Rarity.EPIC)));
+    public static final Item LAVA_CHARM = register("lava_charm", new Item(new FabricItemSettings().fireproof().maxCount(1).rarity(Rarity.UNCOMMON)) {
         @Override
         public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
             for (String line : Text.translatable("item.simplyboots.lava_charm.tooltip").getString().split("\n")) {
